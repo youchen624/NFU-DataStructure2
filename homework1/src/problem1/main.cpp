a46942f0ddffe77a45abcc8a97a8df9845d76c8a
youchen624 <youchen624@gmail.com>
2026-03-29 19:35:55 +0800

    w6a2

---
+++ homework1/src/problem1/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+template<class T>
+class Value2Node {
+    public:
+    T value;
+    Value2Node* left;
+    Value2Node* right;
+};
+*/
+
+template <class T>
+class MinPQ {
+public:
+    virtual ~MinPQ() {};
+    virtual bool empty() {} const = 0;
+    virtual const T& top() const = 0;
+    virtual void push(const T&) = 0;
+    virtual void pop() = 0;
+};
+
+template <class T>
+class MinHeap : public MinPQ {
+public:
+    ~MinHeap() {};
+
+    bool empty() const override {
+        return !_size;
+    };
+
+    const T& top() const override {};
+
+    void push(const T& obj) override {};
+
+    void pop() override {};
+
+    type_t size() const {
+        return _size;
+    };
+
+private:
+    type_t _i
+
+private:
+    type_t _size;
+
+};
+
+int main() {
+    return 0;
+}
